5eb479629fb6f404d45682cd30ff0196574a89fa
Raymond Blum <raygeeknyc@users.noreply.github.com>
2017-04-09 10:05:20 -0400

    Update faceme.py
    Clean up code,  comments

--- faceme.py
+++ faceme.py
@@ -17,24 +17,30 @@ file_name = os.path.join(
     os.path.dirname(__file__),
     sys.argv[1])
 
+# Loads the image into memory
 with io.open(file_name, 'rb') as image_file:
      content = image_file.read()
-
-# Loads the image into memory
+        
+# Send the image to the cloud vision service to a analyze
 image = vision_client.image(content=content)
 
+# Tell the vision service to look for faces in the image
 faces = image.detect_faces(limit=30)
 print "%d faces" % len(faces)
+
+# Create a PIL image that we can draw on
 im = Image.open(io.BytesIO(content))
 draw = ImageDraw.Draw(im) 
+
 frame_color_joy = (128,256,128)
 frame_color_angry = (236,128,128)
 frame_color_meh = (192,192,256)
 angry_faces = 0
 joyful_faces = 0
 for this_face in faces:
-   frame_color = frame_color_meh
    print "Confidence %f" % this_face.detection_confidence
+   # Classify this face as joyful, angry or meh
+   frame_color = frame_color_meh
    if this_face.joy is Likelihood.VERY_LIKELY or this_face.joy is Likelihood.LIKELY:
        print "joy"
        frame_color = frame_color_joy
@@ -43,13 +49,15 @@ for this_face in faces:
        print "anger"
        frame_color = frame_color_angry
        angry_faces += 1
+   # Draw a frame around this face's bounding polygon
    first = this_face.bounds.vertices[0]
    start = first
    for end in this_face.bounds.vertices[1:]:
-       print "(%d,%d - %d,%d)" % (start.x_coordinate, start.y_coordinate, end.x_coordinate, end.y_coordinate)
        draw.line((start.x_coordinate,start.y_coordinate, end.x_coordinate, end.y_coordinate), fill=frame_color, width=2)
        start = end
    draw.line((start.x_coordinate,start.y_coordinate, first.x_coordinate, first.y_coordinate), fill=frame_color, width=2)
+
+# Show the image with highlights and report on the majority of moods detected
 im.show()
 if joyful_faces and joyful_faces > angry_faces:
    print "YAY!!! A happy scene!"
